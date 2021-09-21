e0596d8ab172f114513e3447f7a99844d30295ad
Swarup Kharul <swarupkharulsk@gmail.com>
2021-09-21 22:05:44 +0530

    feat: added fastapi

---
+++ app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request, Form
+from starlette.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:8000",
+        "http://0.0.0.0:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+
+# get method with query parameter
+@app.get("/")
+async def root():
+    return "Ok Working!"
+
+# post method function
+@app.post("/submit")
+async def post_root(request: str = Form(...)):
+    """ To submit request """
+    # msg = request.body()
+    return "Hello " + request
